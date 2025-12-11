144e60e6e8fd2732f0faaee45f4e39683e394c9f
ismaelce122 <ismaelce@hotmail.com>
2025-12-10 21:23:38 -0300

    Cadastrar Veículos

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+
+
+
+@app.route("/cadastrarveiculos")
+def CadastrarVeiculos():
+    return render_template("cadastrarveiculos.html")
+
 # Executa o servidor SOMENTE localmente
 if __name__ == "__main__":
     app.run(debug=True)
