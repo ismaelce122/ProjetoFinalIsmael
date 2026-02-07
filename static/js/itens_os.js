4069658f6a5b7370461dd5e1f213d05caeef0aab
ismaelce122 <ismaelce@hotmail.com>
2026-02-07 19:19:46 -0300

    Ajustando Responsividade

--- static/js/itens_os.js
+++ static/js/itens_os.js
@@ -17,14 +17,14 @@ const AddItem = () => {
     box.id = 'item-' + contador
     box.classList.add('row')
     box.innerHTML = `
-                    <div class="col-4">
+                    <div class="col-4 mt-2">
                         <label class="form-label" for="peca">Peça:</label>
                         <select name="peca[]" class="opcoes form-select">
                             <option value="">-- selecione a peça --</option>
                             +${html}
                         </select>
                     </div>
-                    <div class="col-4">
+                    <div class="col-4 mt-2">
                         <label class="form-label" for="quantidade[]">Quantidade:</label>
                         <input class="form-control" type="number" name="quantidade[]">
                     </div>
@@ -38,7 +38,39 @@ const AddItem = () => {
     item.appendChild(box)
 }
 
+const AddItem2 = () => {
+    contador++
+    const item = document.getElementById('itens_os_celular')
+    const box = document.createElement('div')
+    box.id = 'item-' + contador
+    box.classList.add('row')
+    box.innerHTML = `
+                    <div class="col-12 mt-2">
+                        <label class="form-label" for="peca">Peça:</label>
+                        <select name="peca[]" class="opcoes form-select">
+                            <option value="">-- selecione a peça --</option>
+                            +${html}
+                        </select>
+                    </div>
+                    <div class="col-4 mt-2">
+                        <label class="form-label" for="quantidade[]">Quantidade:</label>
+                        <input class="form-control" type="number" name="quantidade[]">
+                    </div>
+                    <div class="col-4 d-flex align-items-end">
+                        <button type="button" onclick="ExcluirItem2('item-${contador}')" class="btn btn-danger">
+                            <i class="fa fa-trash"></i>
+                        </button>
+                    </div>
+                    `
+    item.appendChild(box)
+}
+
 const ExcluirItem = (id) => {
+    const item = document.getElementById(id)
+    item.remove(item)
+}
+
+const ExcluirItem2 = (id) => {
     const item = document.getElementById(id)
     item.remove(item)
 }
