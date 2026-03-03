fd514374e8fe5965dd89cf037c887156d95bd1a7
ismaelce122 <ismaelce@hotmail.com>
2026-03-03 15:58:43 -0300

    Regulando o Scanner

--- routes/scanner/scanner_static/js/scan.js
+++ routes/scanner/scanner_static/js/scan.js
@@ -1,13 +1,15 @@
-let texto = ''
+let texto = []
 let box = document.getElementById('resultado')
 
 const Scannear = (decodedText, decodedResult) => {
-    document.getElementById("beep").play()
-    let resultado = document.createElement('h2')
-    texto = decodedText
-    resultado.textContent = texto
-    box.appendChild(resultado)
-    texto = ''
+    if (!texto.includes(decodedText)) {
+        texto = []
+        document.getElementById("beep").play()
+        let resultado = document.createElement('h2')
+        texto.push(decodedText)
+        resultado.textContent = texto
+        box.appendChild(resultado)
+    }
 }
 
 let html5QrCode = new Html5Qrcode("reader")
