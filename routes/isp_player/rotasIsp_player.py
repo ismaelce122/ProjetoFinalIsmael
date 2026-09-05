0e21d0babf91c3ef94c641d63ed2edc72103b207
ismaelce122 <143747685+ismaelce122@users.noreply.github.com>
2026-09-05 01:32:38 -0300

    Update rotasIsp_player.py

--- routes/isp_player/rotasIsp_player.py
+++ routes/isp_player/rotasIsp_player.py
@@ -1,4 +1,5 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, Response
+import requests
 
 ispPlayer_bp = Blueprint("ispPlayer", __name__, template_folder='ispPlayer_templates', static_folder='ispPlayer_static')
 
@@ -17,3 +18,22 @@ def politicaIngles():
 @ispPlayer_bp.route("/isp_player/privacy-policy-ko")
 def politicaKoreano():
     return render_template('politica_koreano.html')
+
+@ispPlayer_bp.route("/isp_player/proxy")
+def Proxy():
+    url = request.args.get("url")
+    if not url:
+        return {"error": "URL não fornecida"}, 400
+
+    # Faz a requisição ao servidor IPTV
+    resp = requests.get(url)
+
+    # Cria resposta com os dados recebidos
+    response = Response(resp.content, status=resp.status_code)
+
+    # Adiciona cabeçalhos CORS para liberar no navegador
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+
+    return response
